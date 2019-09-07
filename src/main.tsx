ba32256f187b378b10454b94e9868e61d9ea330a
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-09-07 16:23:53 -0700

    Don't use const case for mount point

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,10 @@ import 'sanitize.css';
 import App from './app';
 
 // get the dom element to mount our application at
-const MOUNT_NODE = document.getElementById('app');
+const appMountPoint = document.getElementById('app');
 
 const render = () => {
-  ReactDOM.render(<App />, MOUNT_NODE);
+  ReactDOM.render(<App />, appMountPoint);
 };
 
 render();
