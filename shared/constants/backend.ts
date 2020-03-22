18bfa9e6867eaa43d17846973f1be3f7f1084426
Sam Hinshaw <samuel.hinshaw@gmail.com>
2020-03-22 14:18:38 -0700

    Remove unused types

--- shared/constants/backend.ts
+++
@@ -1,2 +0,0 @@
-export const BACKEND_PORT = 9924;
-export const API_URL = `http://localhost:${BACKEND_PORT}`;
--- shared/models/list-item.ts
+++
@@ -1,6 +0,0 @@
-export interface ListItem {
-  id: number;
-  title: string;
-  description?: string;
-  ranking: number;
-}
